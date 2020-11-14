4437bf5a857d89ff029f506c468de38c94792d71
Joni Sadler <joni.anne.sadler@gmail.com>
2020-11-14 12:42:58 -0500

    Exercise 2 complete

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,6 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
-
-
 const App = (props) => {
 
   return (
--- src/components/Typeahead.js
+++ src/components/Typeahead.js
@@ -4,6 +4,11 @@ import styled from 'styled-components';
 
 import App from './App';
 
+Exerconst Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
 const UserInput = styled.input` 
   margin: 20px 10px;;
   height: 35px;
@@ -22,23 +27,68 @@ const Button = styled.button`
   font-size: 16px;
 `
 
+const Suggestion = styled.li` 
+ list-style-type: disc;
+ margin-left: 5%;
+ padding: 5px;
+
+ &:hover {
+    background-color: lightyellow;
+ }
+`
+
+const SuggestionContainer = styled.ul` 
+    &:hover {
+        box-shadow: 2px 2px 8px 8px lightgrey;
+        border-radius: 5px;
+        padding: 10px;
+    }
+`
+
 const Typeahead = ({ suggestions, handleSelect }) => {
     const [value, setValue] = useState('');
+    const [matchedSuggestions, setMatchedSuggestions] = useState([]);   
+
+    const getMatches = (suggestions, newValue) => {
+        if (newValue.length > 1) {
+            return suggestions.filter(suggestion => suggestion.title.toLowerCase().includes(newValue.toLowerCase()));
+        } 
+        return [];
+    }
 
     return (
-        <div>
-            <UserInput
-            type='text'
-            value={value}
-            onChange={(ev) => setValue(ev.target.value)}
-            onKeyDown={(ev) => {
-                if (ev.key === 'Enter') {
-                    handleSelect(ev.target.value);
-                }
-            }}
-            />
-            <Button onClick={() => setValue('')}>Clear</Button>
-        </div>
+        <Container>
+            <div>
+                <UserInput
+                type='text'
+                value={value}
+                onChange={(ev) => {
+                    const newValue = ev.target.value;
+                    setValue(newValue);
+                    setMatchedSuggestions(getMatches(suggestions, newValue));
+                }}
+                onKeyDown={(ev) => {
+                    if (ev.key === 'Enter') {
+                        handleSelect(ev.target.value);
+                    }
+                }}
+                />
+                <Button onClick={() => setValue('')}>Clear</Button>
+            </div>
+            <SuggestionContainer>
+                {matchedSuggestions.map((suggestion) => {
+                    return (
+                        <Suggestion
+                            key={suggestion.id}
+                            onClick={() => handleSelect(suggestion.title)}
+                        >
+                            {suggestion.title}
+                        </Suggestion>
+                    );
+                })}
+            </SuggestionContainer>
+            
+        </Container>
     )
 }
 
