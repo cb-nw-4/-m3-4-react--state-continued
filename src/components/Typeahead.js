97a7bd568bccb2d5a67c0ed379606f6a2fe54dd4
ferielnabet <fnabet@ipconnex.com>
2020-11-14 17:26:48 -0500

    Add gouping Category && escape part

--- src/components/Typeahead.js
+++ src/components/Typeahead.js
@@ -7,24 +7,29 @@ import styled, { css } from 'styled-components';
 
 const Typeahead = ({suggestions, handleSelect}) => {
     const [value, setValue] = React.useState('');
-    const [selectedSUggestionIndex, setSelectedSuggestionIndex] = React.useState(0);
+    const [selectedSuggestionIndex, setSelectedSuggestionIndex] = React.useState(0);
+    
+    const[isEscape, setEscape] =React.useState(false);
     
     let firstHalf, secondHalf;
     let categories = data.categories;
+    const groupedSuggestions = [];
 
     const matchedSuggestions = suggestions.filter(suggestion =>{
-
-        let containValue = suggestion.title.toLowerCase().includes(value);        
+        
+        let containValue = suggestion.title.toLowerCase().includes(value);   
         if(value.length >=2 && containValue){
             return (suggestion.title)
-        }
-        
+        }        
     });
 
 
+    Object.values(categories).map(value => {
+        let result = matchedSuggestions.filter(suggestion => (suggestion.categoryId ===value.id))
+        groupedSuggestions.push({'categoryName': value.name, suggestions: result})
+    })
     return ( 
         <Wrapper>
-
             <div>
                 <Input
                     type='text'
@@ -38,14 +43,26 @@ const Typeahead = ({suggestions, handleSelect}) => {
                                 return;
                             }
                             case 'ArrowUp':{
-                                setSelectedSuggestionIndex(selectedSUggestionIndex -1);
-                                return;
-
+                                if(selectedSuggestionIndex >= 1){
+                                    setSelectedSuggestionIndex(selectedSuggestionIndex -1);
+                                    return;
+                                }
                             }
                             case 'ArrowDown':{
-                                setSelectedSuggestionIndex(selectedSUggestionIndex + 1);
+                                setSelectedSuggestionIndex(selectedSuggestionIndex + 1);
                                 return;
                             }
+
+                            case 'Escape':{
+                                setEscape(true);
+                                return;
+                            }
+
+                            default :{
+                                setEscape(false);
+                                return;
+                            }
+
                         }
                     }}
                 />
@@ -57,45 +74,70 @@ const Typeahead = ({suggestions, handleSelect}) => {
             </div>
 
             {matchedSuggestions.length > 0 &&
-                <Ul>
-                    {matchedSuggestions.map((suggestion, i) =>{
-                        let indexOfValue = suggestion.title.toLowerCase().indexOf(value);
-            
-                        firstHalf = suggestion.title.slice(0, indexOfValue + value.length);
-                        secondHalf = suggestion.title.slice(indexOfValue + value.length);
-                    
-                        return(
-                            <Suggestion
-                                key={suggestion.id}
-                                onClick={()=> handleSelect(suggestion.title)}
+                <Ul>{groupedSuggestions.map((groupedSuggestion) =>{
+
+                    return(
+
+                        groupedSuggestion.suggestions.length> 0 &&
                             
-                                // className= {selectedSUggestionIndex === i ? "selected" : null }
-                                onMouseOver={() => {
-                                    setSelectedSuggestionIndex(i)
-                                }}
-                                
-                                selected= {selectedSUggestionIndex === i}
+                            <li key={groupedSuggestion.categoryName}>
+
+                                <span className='title'>In {groupedSuggestion.categoryName} :</span>
+
+                                <ul>
+                                    {groupedSuggestion.suggestions.map((suggestion, i) =>{
+
+                                        let indexOfValue = suggestion.title.toLowerCase().indexOf(value);
+
+                                        firstHalf = suggestion.title.slice(0, indexOfValue + value.length);
+                                        secondHalf = suggestion.title.slice(indexOfValue + value.length);
+                                        
+                                        return(
+
+                                            <Suggestion
+                                                key={suggestion.id}
+                                                onClick={()=> handleSelect(suggestion.title)}
+                        
+                                                className= {isEscape ? "expended-area" : null }
+
+                                                onMouseOver={() => {
+                                                    setSelectedSuggestionIndex(i)
+                                                }}
+
+                                                selected= {selectedSuggestionIndex === i}
+                            
+                                            >
+                                                <span>
+                                                    {firstHalf}
+                                                    <Prediction>
+                                                        {secondHalf}
+                                                    </Prediction>
+
+                                                    <span className='spaceIn'>
+                                                        in
+                                                    </span>
+
+                                                    <span>
+                                                        {categories[suggestion.categoryId].name}
+                                                    </span>
+                                                </span>
+                                            </Suggestion>
+                                            
+                                            
+                                        )
+                                })}</ul>
+                        
+                    
+                            </li>)
+
+
+                })}</Ul>
+
                 
-                            >
-                                <span>
-                                    {firstHalf}
-                                    <Prediction>
-                                        {secondHalf}
-                                    </Prediction>
-
-                                    <span className='spaceIn'>
-                                        in
-                                    </span>
-
-                                    <span>
-                                        {categories[suggestion.categoryId].name}
-                                    </span>
-                                </span>
-                            </Suggestion>
-                        )
-                    })}
-                </Ul>
             }
+
+
+
 
     </Wrapper>
     );
@@ -106,14 +148,21 @@ const Wrapper = styled.div `
     display: flex;
     flex-direction:column;
     align-items: center;
-    margin: 50px;
+    justify-content: center;
+    margin: 100px;
+
+
+    & div{
+        display: flex;
+        width: 70%;
+    }
 `
 
 const Button = styled.button`
     padding: 10px 20px;
     background-color: blue;
     color: white;
-    font-size: 20px;
+    font-size: 24px;
     border-radius: 5px;
     border: none;
 
@@ -123,6 +172,7 @@ const Input = styled.input`
     
     font-size: 18px;
     padding: 10px; 
+    flex-grow: 2;
     
     &:active{
         border: 3px solid blue
@@ -136,7 +186,14 @@ const Ul = styled.ul`
     -moz-box-shadow: -5px 8px 29px 26px rgba(194,194,194,0.64);
     margin: 10px;
     border: none;
-    width: 50%;
+    width: 70%;
+
+    .title {
+        color: gray;
+        display: block;
+        padding: 20px;
+        margin: 20px
+    }
 `
 
 
@@ -147,6 +204,10 @@ const Suggestion = styled.li`
 
     &.selected {
         background: hsla(50deg, 100%, 80%, 0.25);
+    }
+
+    &.expended-area{
+        display: none;
     }
 
     .spaceIn{
