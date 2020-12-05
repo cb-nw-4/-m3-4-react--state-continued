43b7f2cc059f3a9a11d764a57967a415ef5dde03
Sonitta Phet <sonitta.phet@gmail.com>
2020-12-05 09:57:24 -0500

    Answered exercises

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import data from '../data';
 
 import GlobalStyles from './GlobalStyles';
+import Typeahead from './Typeahead';
 
 const App = (props) => {
   return (
     <>
       <GlobalStyles />
-      {/* TODO */}
+
+        <Typeahead
+          suggestions ={data.books}
+          handleSelect={(suggestion) =>{
+            window.alert(suggestion)
+          }}
+        
+        />
+
+    
     </>
   );
 };
 
 export default App;
+
